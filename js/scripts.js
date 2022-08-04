aff7c419dece55367fd1c116f578e97c2923c479
sbmccauley <60712389+sbmccauley@users.noreply.github.com>
2022-08-03 17:30:49 -0700

    Adjusted Event Listener Type

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 let newEmp = document.getElementById('empForm');
 
 
-newEmp.addEventListener('click', (e) => {
-       
+newEmp.addEventListener('submit', (e) => {
+    e.preventDefault();
     console.log(`ID: ${document.getElementById('id').value}`);
     console.log(`Name: ${document.getElementById('name').value}`);
     console.log(`Extension: ${document.getElementById('ext').value}`);
     console.log(`Email: ${document.getElementById('email').value}`);
     console.log(`Department: ${document.getElementById('department').value}`);
-    e.preventDefault();
+   
 })
 
